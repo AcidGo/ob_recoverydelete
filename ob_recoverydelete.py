8a4a9fe29d801106a3be26bbbcc5997717cb019d
AcidGo <zhangruijun95@163.com>
2020-08-05 12:56:37 +0800

    conver to cmd line mode

--- ob_recoverydelete.py
+++ ob_recoverydelete.py
@@ -2,35 +2,43 @@
 from __future__ import print_function
 import re
 import json
+import sys
 
 
-re_p = re.compile(r'OLD_ROW=\{(.*?)\} \|')
-re_p_0 = re.compile(r'([0-9]+):\{(.*?)\}')
-re_p_1 = re.compile(r', .*$')
-re_p_2 = re.compile(r'".*?":(.*?)$')
-row_lst = []
-with open("1107208209222809.all.dump", 'r') as f:
-    for i in f:
-        row_lst.append(i.strip())
-        
-for line in row_lst:
-    re_res = re_p.findall(line)
-    for jj in re_res:
-        if jj.strip() == '{}': continue
-        r_str = jj.strip()
-        re_jj = re_p_0.findall(r_str)
-        is_one = True
-        is_in = False
-        for jjj in re_jj:
-            if not is_one:
-                print(", ", end="")
-            else:
-                print("(", end="")
-                is_one = False
-            data = re_p_1.sub("", jjj[1]).strip()
-            data = re_p_2.search(data)
-            data = data.group(1)
-            print(data, end="")
-            is_in = True
-        if is_in:
-            print(");")
+def main()
+    if len(sys.argv) != 2:
+        raise ValueError("invalid~~~")
+    clog_dump_file = sys.argv[1]
+    re_p = re.compile(r'OLD_ROW=\{(.*?)\} \|')
+    re_p_0 = re.compile(r'([0-9]+):\{(.*?)\}')
+    re_p_1 = re.compile(r', .*$')
+    re_p_2 = re.compile(r'".*?":(.*?)$')
+    row_lst = []
+    with open(clog_dump_file, 'r') as f:
+        for i in f:
+            row_lst.append(i.strip())
+            
+    for line in row_lst:
+        re_res = re_p.findall(line)
+        for jj in re_res:
+            if jj.strip() == '{}': continue
+            r_str = jj.strip()
+            re_jj = re_p_0.findall(r_str)
+            is_one = True
+            is_in = False
+            for jjj in re_jj:
+                if not is_one:
+                    print(", ", end="")
+                else:
+                    print("(", end="")
+                    is_one = False
+                data = re_p_1.sub("", jjj[1]).strip()
+                data = re_p_2.search(data)
+                data = data.group(1)
+                print(data, end="")
+                is_in = True
+            if is_in:
+                print(");")
+
+if __name__ == "__main__":
+    main()
